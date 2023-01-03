32c988c18ec10f52abee44730e5c9d99f437dc95
Sören Poppe <soeren.poppe@thedrama.de>
2023-01-03 15:33:28 +0100

    add picture arrays to picture header

--- Pflanzensensor.X/picture.h
+++ Pflanzensensor.X/picture.h
@@ -2,8 +2,13 @@
 #ifndef PICTURE_H_
 #define PICTURE_H_
 
-extern const __flash int Bild1[2301];
-extern const __flash int Bild1Animated[2085];
+extern const __flash int Bild1[2085];
+extern const __flash int Bild1Animated[2025];
+extern const __flash int Bild2[1929];
+extern const __flash int Bild2Animated[1869];
+extern const __flash int Bild3[1978];
+extern const __flash int Bild3Animated[1908];
+extern const __flash int Status[891];
 
 
 #endif /* PICTURE_H_ */
