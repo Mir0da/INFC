ef37ba7dae9937037a974ccaf1fccf35fa37b0ba
Sören Poppe <soeren.poppe@thedrama.de>
2023-01-03 15:22:21 +0100

    add pictures and different windows to paint

--- Pflanzensensor.X/main.c
+++ Pflanzensensor.X/main.c
@@ -93,9 +93,29 @@ int main(void) {
     
     uint8_t buttonFlag=0;
 
-//    uint16_t window[] ={
+//    uint16_t plantwindow[] ={
 //        0xEF08, 0x1805, //Initialisierungsstart, Landscape Modus
-//        0x127E, 0x1505, 0x1305, 0x16AA //Werte f�r Fenstergr��e, xAnfang (0x1267), xEnde, yStart, yEnde (0x169D)       
+//        0x1269, 0x1505, 0x1305, 0x16AA //Werte fuer Fenstergroesse, xEnde(106), xAnfang(6), yStart(6), yEnde (171)       
+//	};
+//    uint16_t statuswindow[] ={
+//        0xEF08, 0x1805, //Initialisierungsstart, Landscape Modus
+//        0x127E, 0x156A, 0x1305, 0x16AA //Werte fuer Fenstergroesse, xEnde(127), xAnfang(107), yStart(6), yEnde (171)       
+//	};
+//    uint16_t sleepwindow[] ={
+//        0xEF08, 0x1805, //Initialisierungsstart, Landscape Modus
+//        0x1279, 0x156F, 0x1341, 0x164B //Werte fuer Fenstergroesse, xEnde(122), xAnfang(112), yStart(66), yEnde (76)       
+//	};
+//    uint16_t melody1window[] ={
+//        0xEF08, 0x1805, //Initialisierungsstart, Landscape Modus
+//        0x1279, 0x156F, 0x1373, 0x167D //Werte fuer Fenstergroesse, xEnde(122), xAnfang(112), yStart(116), yEnde (126)       
+//	};
+//    uint16_t melody2window[] ={
+//        0xEF08, 0x1805, //Initialisierungsstart, Landscape Modus
+//        0x1279, 0x156F, 0x1385, 0x168F //Werte fuer Fenstergroesse, xEnde(122), xAnfang(112), yStart(134), yEnde (144)       
+//	};
+//    uint16_t melody3window[] ={
+//        0xEF08, 0x1805, //Initialisierungsstart, Landscape Modus
+//        0x1279, 0x156F, 0x1397, 0x16A1 //Werte fuer Fenstergroesse, xEnde(122), xAnfang(112), yStart(152), yEnde (162)       
 //	};
 //    
 //    for(uint16_t i= 0; i < (176*132); i++)
@@ -104,7 +124,7 @@ int main(void) {
 //       SPISend8Bit(0x00); // SCHWARZ 0x0000
 //    }
 //    
-//    SendCommandSeq(window,6);
+//    SendCommandSeq(plantwindow,6);
 //    
 //    for(uint16_t i= 0; i < (166*122); i++)
 //    {
